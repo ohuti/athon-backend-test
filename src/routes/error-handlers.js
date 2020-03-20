dcc27d09cdc4e788c864c7cc41856af6978afbad
Andre Ohuti <ohuti@hotmail.com>
2020-03-20 16:47:02 -0300

    Reorganized routes error handlers

---
+++ src/routes/error-handlers.js
@@ -0,0 +1,21 @@
+export const notFoundHandler = (request, response) => response.status(404).json({ status: 404, response: 'not_found' })
+
+export const databaseRuntimeError = (error, request, response, next) => {
+  if(error.errno){
+    console.error('[*] ', { ...error })
+    response.status(500).json({ status: 500, response: 'internal_server_error' })
+    return
+  }
+
+  next(error)
+}
+
+export const exceptionHandler = (error, request, response, next) => {
+  if(typeof error.status === 'number'){
+    response.status(error.status).json({ ...error })
+    return
+  }
+
+  console.error('[+]', error)
+  response.status(500).json({ status: 500, response: 'internal_server_error' })
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 
 import weaponRouter from './weapon'
 import crimeRouter from './crime'
+import { notFoundHandler, databaseRuntimeError, exceptionHandler } from './error-handlers'
 
 const mainRouter = express.Router()
 
@@ -10,26 +11,8 @@ mainRouter.get('/', (request, response) => response.status(200).json({ status: '
 mainRouter.use('/weapon', weaponRouter)
 mainRouter.use('/crime', crimeRouter)
 
-mainRouter.use((request, response) => response.status(404).json({ status: 404, response: 'not_found' }))
-
-mainRouter.use((error, request, response, next) => {
-  if(error.errno){
-    console.error('[*] ', { ...error })
-    response.status(500).json({ status: 500, response: 'internal_server_error' })
-    return
-  }
-
-  next(error)
-})
-
-mainRouter.use((error, request, response, next) => {
-  if(typeof error.status === 'number'){
-    response.status(error.status).json({ ...error })
-    return
-  }
-
-  console.error('[+]', error)
-  response.status(500).json({ status: 500, response: 'internal_server_error' })
-})
+mainRouter.use(notFoundHandler)
+mainRouter.use(databaseRuntimeError)
+mainRouter.use(exceptionHandler)
 
 export default mainRouter
