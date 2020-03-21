97dceab2053af740cc1cd618671762c3282ea04e
Andre Ohuti <ohuti@hotmail.com>
2020-03-21 17:54:54 -0300

    Made create crime body parameters required

--- src/routes/crime/joi-schema.js
+++ src/routes/crime/joi-schema.js
@@ -4,16 +4,16 @@ export const addCrimeSchema = joi.object({
   params: joi.object({ }).required(),
   query: joi.object({ }).required(),
   body: joi.object({
-    victims: joi.array().items(joi.string().max(45)).error(() => `Victim's name must contain less than 45 characters.`),
+    victims: joi.array().items(joi.string().max(45)).required().error(() => `Victim's name is required and must contain less than 45 characters.`),
     weapons: joi.array().items(
       { 
-        weapon: joi.string().max(45).error(() => `Weapon's name must contain less than 45 characters.`),
-        weapon_type: joi.string().max(45).error(() => `Weapon's type must contain less than 45 characters.`)
+        weapon: joi.string().max(45).error(() => `Weapon's name is required and must contain less than 45 characters.`),
+        weapon_type: joi.string().max(45).error(() => `Weapon's type is required and must contain less than 45 characters.`)
       }
-    ),
-    criminals: joi.array().items(joi.string().max(45)).error(() => `Criminal's name must contain less than 45 characters.`),
-    crime_type: joi.array().items(joi.string().max(45)).error(() => `Crime's type must contain less than 45 characters.`),
-    country: joi.string().max(45).error(() => `Country's name must contain less than 45 characters.`)
+    ).required(),
+    criminals: joi.array().items(joi.string().max(45)).required().error(() => `Criminal's name is required and must contain less than 45 characters.`),
+    crime_type: joi.array().items(joi.string().max(45)).required().error(() => `Crime's type is required and must contain less than 45 characters.`),
+    country: joi.string().max(45).required().error(() => `Country's name is required and must contain less than 45 characters.`)
   }).required()
 })
 
