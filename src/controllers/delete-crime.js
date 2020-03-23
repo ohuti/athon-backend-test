98b557f1e14f17b4578b68e884dcbaa550dcc8c7
Andre Ohuti <ohuti@hotmail.com>
2020-03-22 23:02:21 -0300

    Renamed Delete crime controller and added a treatment at used weapons if it finds nothing in database

--- src/controllers/delete-crime.js
+++ src/controllers/delete-crime.js
@@ -1,16 +1,20 @@
 import findCrime from "../database/queries/crime/find-crime"
 import deleteCrime from "../database/queries/crime/delete-crime"
 
-const deleteCrimeAndReferences = async (request, response) => {
+const executeDeleteCrime = async (request, response) => {
   const { country, date } = request.query
   const reference = country ?
     `tx_country = '${country}'` :
     `dt_crime between '${date} 00:00:00' and '${date} 23:59:59'`
   
   const crimeIds = await findCrime(reference)
+  if(!crimeIds) {
+    const message = country ? `in ${country}` : `at ${date}`
+    throw { status: 200, response: 'crime(s)_not_found', message: `no crime was found ${message} in database, therefore couldn't be deleted.` }
+  }
   crimeIds.forEach(async crimeId => await deleteCrime(crimeId))
 
   return response.status(200).json({ status: 200, response: 'OK' })
 }
 
-export default deleteCrimeAndReferences
+export default executeDeleteCrime
--- src/controllers/used-weapons.js
+++ src/controllers/used-weapons.js
@@ -3,6 +3,10 @@ import selectUsedWeapons from "../database/queries/weapon/find-used-weapons"
 const executeUsedWeapon = async (request, response) => {
   const weapons = await selectUsedWeapons()
 
+  if(!weapons){
+    throw { status: 200, response: 'used_weapons_not_found', message: 'not used weapon were found in database.' }
+  }
+
   return response.status(200).json({
     status: 200,
     response: 'OK',
